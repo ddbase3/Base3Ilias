f4942185927b2ed17ffb6480819dcccd1201c0a3
qualitus-dahme <Dahme@qualitus.de>
2025-06-29 22:22:03 +0200

    directory constants

--- resources/base3.php
+++ resources/base3.php
@@ -1,13 +1,27 @@
 <?php declare(strict_types=1);
 
-define('DIR_ILIAS', realpath(__DIR__ . '/..') . DIRECTORY_SEPARATOR);
-define('DIR_TMP', '/srv/www/data/ilias10/base3/'); // TODO: in config auslagern
-define('DIR_SRC', DIR_ILIAS . 'components/Base3/Base3Framework/src/');
-define('DIR_TEST', DIR_ILIAS . 'components/Base3/Base3Framework/test/');
-define('DIR_PLUGIN', DIR_ILIAS . 'components/Base3/');
+define('DIR_ILIAS', realpath(__DIR__ . '/..') . '/');
+
+$iliasConfig = parse_ini_file(DIR_ILIAS . 'ilias.ini.php', true);
+$dataDir = $clientDir = $tmpDir = '';
+if (isset($iliasConfig['clients']) && isset($iliasConfig['clients']['datadir']) && isset($iliasConfig['clients']['default'])) {
+	$dataDir = $iliasConfig['clients']['datadir'] . '/';
+	$clientDir = $dataDir . $iliasConfig['clients']['default'] . '/';
+	$tmpDir = $clientDir . 'base3/';
+}
+define('DIR_DATA', $dataDir);
+define('DIR_CLIENT', $clientDir);
+define('DIR_TMP', $tmpDir);
+
+define('DIR_COMPONENTS', DIR_ILIAS . 'components/');
+define('DIR_BASE3', DIR_COMPONENTS . 'Base3/');
+define('DIR_FRAMEWORK', DIR_BASE3 . 'Base3Framework/');
+define('DIR_SRC', DIR_FRAMEWORK . 'src/');
+define('DIR_TEST', DIR_FRAMEWORK . 'test/');
+define('DIR_PLUGIN', DIR_BASE3);
 define('DIR_LOCAL', DIR_TMP);
 
 require_once DIR_ILIAS . 'vendor/composer/vendor/autoload.php';
 
-include DIR_ILIAS . 'components/Base3/Base3Ilias/src/Base3IliasBootstrap.php';
+include DIR_BASE3 . 'Base3Ilias/src/Base3IliasBootstrap.php';
 (new \Base3Ilias\Base3IliasBootstrap())->run();
