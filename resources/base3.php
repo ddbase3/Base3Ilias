6d9c5499e50e36bf2ee3da365f4aa53859d39007
qualitus-dahme <Dahme@qualitus.de>
2025-06-29 22:27:23 +0200

    comments

--- resources/base3.php
+++ resources/base3.php
@@ -1,7 +1,9 @@
 <?php declare(strict_types=1);
 
+// ILIAS base dir
 define('DIR_ILIAS', realpath(__DIR__ . '/..') . '/');
 
+// Generate data dir and tmp dir from ini (Remember to create DIR_TMP!)
 $iliasConfig = parse_ini_file(DIR_ILIAS . 'ilias.ini.php', true);
 $dataDir = $clientDir = $tmpDir = '';
 if (isset($iliasConfig['clients']) && isset($iliasConfig['clients']['datadir']) && isset($iliasConfig['clients']['default'])) {
@@ -13,6 +15,7 @@ define('DIR_DATA', $dataDir);
 define('DIR_CLIENT', $clientDir);
 define('DIR_TMP', $tmpDir);
 
+// installation dirs
 define('DIR_COMPONENTS', DIR_ILIAS . 'components/');
 define('DIR_BASE3', DIR_COMPONENTS . 'Base3/');
 define('DIR_FRAMEWORK', DIR_BASE3 . 'Base3Framework/');
@@ -21,7 +24,10 @@ define('DIR_TEST', DIR_FRAMEWORK . 'test/');
 define('DIR_PLUGIN', DIR_BASE3);
 define('DIR_LOCAL', DIR_TMP);
 
+// autoload
 require_once DIR_ILIAS . 'vendor/composer/vendor/autoload.php';
 
+// bootstrap
 include DIR_BASE3 . 'Base3Ilias/src/Base3IliasBootstrap.php';
 (new \Base3Ilias\Base3IliasBootstrap())->run();
+
