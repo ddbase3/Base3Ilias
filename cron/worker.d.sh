261232e86ba9fbe63a2161983f356a0d9ce8301a
qualitus-dahme <Dahme@qualitus.de>
2026-05-18 15:34:09 +0200

    worker changed for new endpoint architecture

--- cron/worker.d.sh
+++ cron/worker.d.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-cd ../../../../public || exit 1
+URL='https://ddahme.qualitus.net/reporting10/ilias.php?baseClass=ilUIPluginRouterGUI&cmdClass=ilBase3IliasAdapterAjaxGUI&cmd=dispatch&name=masterworker'
+LOG='/var/log/base3-worker.log'
 
 while true
 do
-        php base3.php -f --name=masterworker
-        sleep 1
-done
+	echo "[$(date '+%Y-%m-%d %H:%M:%S')] start masterworker" >> "$LOG"
 
+	curl -k -fsS \
+		--connect-timeout 10 \
+		--max-time 0 \
+		"$URL" >> "$LOG" 2>&1
+
+	STATUS=$?
+
+	echo "[$(date '+%Y-%m-%d %H:%M:%S')] done masterworker status=${STATUS}" >> "$LOG"
+
+	sleep 1
+done
